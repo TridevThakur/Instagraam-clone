200f85ac00407af0ed7477975549f90c866cf81d
TRIDEV THAKUR <56440480+TridevThakur@users.noreply.github.com>
2022-09-20 17:02:51 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+
+module.exports= {
+    MONGODB_URI:"mongodb+srv://myappupdate:SxAzuTlSVLByXVOe@cluster0.zocifof.mongodb.net/mernstack?retryWrites=true&w=majority",
+    JWT_SECRET:"hmlkjidssxcjcjskiue"
+}a
